67456a29ef6bbfddbb707ae5ecacf3ffaa275faa
Poojitha <poojitha032320@gmail.com>
2022-10-07 18:39:14 +0530

    testing

---
+++ sample.py
@@ -0,0 +1,7 @@
+from flask import Flask
+from healthcheck import HealthCheck
+
+app = Flask(__name__)
+
+health = HealthCheck()
+app.add_url_rule("/healthcheck", "healthcheck", view_func=lambda: health.run())
